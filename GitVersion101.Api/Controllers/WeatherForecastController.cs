4f4e5eadaf00f93d3996e4a6475fe170407f118a
Antonios Klimis <antonios.klimis@gmail.com>
2020-06-19 12:36:58 +0300

    ch41 pr workflow

--- GitVersion101.Api/Controllers/WeatherForecastController.cs
+++ GitVersion101.Api/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace GitVersion101.Api.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random(32);
+            var rng = new Random(33);
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
                     Date = DateTime.Now.AddDays(index),
